08a104c4819511a798679dfe18df85aad311d41e
nandobfer <nandobfer@yahoo.com.br>
2025-07-18 19:38:02 -0300

    chore: changed event container to be an accordeon

--- src/pages/Events/EventContainer.tsx
+++ src/pages/Events/EventContainer.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react"
-import { Avatar, Box, Button, ClickAwayListener, Divider, Menu, Tooltip, Typography } from "@mui/material"
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Avatar,
+    Box,
+    Button,
+    ClickAwayListener,
+    Divider,
+    Menu,
+    Tooltip,
+    Typography,
+} from "@mui/material"
 import type { Event } from "../../types/server/class/Event"
 import { BrokenImage, LocationPin, Reply } from "@mui/icons-material"
 import dayjs from "dayjs"
 import { currencyMask } from "../../tools/numberMask"
 import { EventLocation } from "../../components/EventLocation"
 import { WhoPlays } from "../../components/WhoPlays"
+import { GridExpandMoreIcon } from "@mui/x-data-grid"
 
 interface EventContainerProps {
     event: Event
@@ -24,72 +37,85 @@ export const EventContainer: React.FC<EventContainerProps> = (props) => {
 
     return (
         <Box sx={{ flexDirection: "column", gap: 1, width: 1 }}>
-            <Box sx={{ alignItems: "center", justifyContent: "space-between", marginBottom: -1 }}>
-                <Box sx={{ flexDirection: "column", width: 1 }}>
-                    <Typography variant="subtitle2" sx={{ fontWeight: "bold" }}>
-                        {event.title}
-                    </Typography>
-                    <Box sx={{ justifyContent: "space-between", width: 1 }}>
-                        <Typography variant="body2" sx={{ fontWeight: "bold" }} color="primary">
-                            {dayjs(Number(event.datetime)).format("HH:mm")}
-                        </Typography>
-                        <Typography variant="body1" color="success" sx={{ fontWeight: "bold" }}>
-                            {event.price ? currencyMask(event.price) : "GRÁTIS"}
-                        </Typography>
+            <Accordion sx={{ flexDirection: "column" }} slots={{ root: Box }}>
+                <AccordionSummary expandIcon={<GridExpandMoreIcon />} sx={{ padding: 0 }}>
+                    <Box sx={{ alignItems: "center", justifyContent: "space-between" }}>
+                        <Box sx={{ flexDirection: "column", width: 1 }}>
+                            <Box sx={{ justifyContent: "space-between", width: 1, alignItems: "center", gap: 1 }}>
+                                <Typography variant="subtitle2" sx={{ fontWeight: "bold" }} color="primary">
+                                    {dayjs(Number(event.datetime)).format("HH:mm")}
+                                </Typography>
+                                <Typography variant="subtitle2" sx={{ fontWeight: "bold" }}>
+                                    {event.title}
+                                </Typography>
+                            </Box>
+                            <Typography variant="body1" color="success" sx={{ fontWeight: "bold" }}>
+                                {event.price ? currencyMask(event.price) : "GRÁTIS"}
+                            </Typography>
+                        </Box>
                     </Box>
-                </Box>
-            </Box>
-
-            {event.image && (
-                <Avatar
-                    src={event.image || undefined}
-                    sx={{ width: 1, height: 150, bgcolor: "background.paper", color: "primary.main" }}
-                    variant="rounded"
+                </AccordionSummary>
+                <AccordionDetails
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: 1,
+                        padding: 1,
+                        paddingTop: 0,
+                    }}
                 >
-                    <BrokenImage sx={{ width: 1, height: 1 }} />
-                </Avatar>
-            )}
+                    {event.image && (
+                        <Avatar
+                            src={event.image || undefined}
+                            sx={{ width: 1, height: "auto", bgcolor: "background.paper", color: "primary.main" }}
+                            variant="rounded"
+                            slotProps={{ img: { style: { objectFit: "cover" } } }}
+                        >
+                            <BrokenImage sx={{ width: 1, height: 1 }} />
+                        </Avatar>
+                    )}
 
-            <Typography variant="subtitle2">{event.description}</Typography>
+                    <Typography variant="subtitle2">{event.description}</Typography>
 
-            {(event.bands.length > 0 || event.artists.length > 0) && <WhoPlays artists={event.artists} bands={event.bands} />}
+                    {(event.bands.length > 0 || event.artists.length > 0) && <WhoPlays artists={event.artists} bands={event.bands} />}
 
-            {!!event.location.street && (
-                <ClickAwayListener onClickAway={() => setShowLocation(false)}>
-                    <Tooltip
-                        title={<EventLocation location={event.location} />}
-                        open={showLocation}
-                        placement="auto-end"
-                        slotProps={{ tooltip: { sx: { padding: 0, bgcolor: "transparent" } } }}
-                        // arrow={false}
-                    >
+                    {!!event.location.street && (
+                        <ClickAwayListener onClickAway={() => setShowLocation(false)}>
+                            <Tooltip
+                                title={<EventLocation location={event.location} />}
+                                open={showLocation}
+                                placement="auto-end"
+                                slotProps={{ tooltip: { sx: { padding: 0, bgcolor: "transparent" } } }}
+                                // arrow={false}
+                            >
+                                <Button
+                                    size="small"
+                                    onClick={() => setShowLocation(true)}
+                                    sx={{ borderBottom: "1px solid", borderRadius: 0 }}
+                                    // variant="contained"
+                                    endIcon={<LocationPin sx={{ rotate: "180deg", transform: "scale(1, -1)" }} />}
+                                    color="info"
+                                >
+                                    Como chegar
+                                </Button>
+                            </Tooltip>
+                        </ClickAwayListener>
+                    )}
+
+                    {event.ticketUrl && (
                         <Button
                             size="small"
-                            onClick={() => setShowLocation(true)}
+                            onClick={() => window.open(event.ticketUrl!, "_new")}
                             sx={{ borderBottom: "1px solid", borderRadius: 0 }}
                             // variant="contained"
-                            endIcon={<LocationPin sx={{ rotate: "180deg", transform: "scale(1, -1)" }} />}
-                            color="info"
+                            endIcon={<Reply sx={{ rotate: "180deg", transform: "scale(1, -1)" }} />}
                         >
-                            Como chegar
+                            Adquirir ingresso
                         </Button>
-                    </Tooltip>
-                </ClickAwayListener>
-            )}
-
-            {event.ticketUrl && (
-                <Button
-                    size="small"
-                    onClick={() => window.open(event.ticketUrl!, "_new")}
-                    sx={{ borderBottom: "1px solid", borderRadius: 0 }}
-                    // variant="contained"
-                    endIcon={<Reply sx={{ rotate: "180deg", transform: "scale(1, -1)" }} />}
-                >
-                    Adquirir ingresso
-                </Button>
-            )}
-
-            {props.divider && <Divider sx={{ my: 2 }} />}
+                    )}
+                </AccordionDetails>
+            </Accordion>
+            {props.divider && <Divider sx={{}} />}
 
             <Menu open={!!menuAnchor} anchorEl={menuAnchor} onClose={closeMenu}></Menu>
         </Box>
